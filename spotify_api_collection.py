9b6025fa707186856e6547d5984f3efb28b5dd68
BrianLoe <corneliusbrian.loe@students.mq.edu.au>
2023-12-30 23:50:49 +1100

    update requirements, add trending updater

--- spotify_api_collection.py
+++ spotify_api_collection.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 import json
 import requests
@@ -25,6 +26,8 @@ class SpotipyScraper:
         self.oracle_access_token = None
         self.result_dict = None
         
+        response = requests.get("https://g9c989a618c1148-spotifydb.adb.ap-sydney-1.oraclecloudapps.com/ords/admin/api/get-trendings").json()
+        self.current_trending_tracks = pd.DataFrame(response['items'])
         self.artist_ids = []
     
     def _get_spotify_access_token(self):
@@ -82,7 +85,9 @@ class SpotipyScraper:
                         self.result_dict['artist_image_url'].append(None)
                     self.result_dict['artist_url'].append(artist['external_urls']['spotify'])
                     self.result_dict['artist_popularity'].append(artist['popularity'])
-                self.post_to_oracledb('artists')
+                flag = self._check_empty_result_dict()
+                if not flag:
+                    self.post_to_oracledb('artists')
                 time.sleep(3)
                 
     def get_audio_features(self, track_ids):
@@ -130,7 +135,7 @@ class SpotipyScraper:
             'track_image_url':[],
             'track_release_year':[],
             'artist_id':[],
-            'track_rank':[]
+            'track_rank':[],
         }
         if self.client:
             playlist_response = self.client.playlist(playlist_id)['tracks']['items'] 
@@ -153,15 +158,37 @@ class SpotipyScraper:
                     self.result_dict['track_release_year'].append(the_track['album']['release_date'].split('-')[0])
                 self.result_dict['artist_id'].append(the_track['artists'][0]['id'])
                 self.result_dict['track_rank'].append(rank+1)
-            temp_resultdict = self.result_dict.copy()
-    
-            del self.result_dict['track_rank']
-            self.post_to_oracledb('tracks')
-            if update_trending:
-                self.result_dict = {key: temp_resultdict[key] for key in ['track_id', 'track_rank']}
-                self.post_to_oracledb('trending')
-            if store_artist_ids:
-                self.artist_ids += self.result_dict['artist_id']
+            flag = self._check_empty_result_dict()
+            if not flag:
+                temp_resultdict = self.result_dict.copy()
+                if store_artist_ids:
+                    self.artist_ids += self.result_dict['artist_id']
+                del self.result_dict['track_rank']
+                self.post_to_oracledb('tracks')
+                if update_trending:
+                    temp_dict = {key: temp_resultdict[key] for key in ['track_id', 'track_rank']}
+                    if self.current_trending_tracks.empty:
+                        temp_dict['status'] = ['still'] * len(temp_dict['track_id'])
+                        self.result_dict = temp_dict
+                    else:
+                        current_set = set(self.current_trending_tracks['track_id'].tolist())
+                        new_set = set(temp_resultdict['track_id'])
+                        new_tracks = new_set - current_set
+                        new_df = pd.DataFrame(temp_dict)
+                        # Filtering existing tracks
+                        temp_df = pd.merge(self.current_trending_tracks, new_df, on='track_id', how='left', suffixes=('_old','_new'))
+                        temp_df['rank_diff'] = temp_df['track_rank_new'] - temp_df['track_rank_old']
+                        temp_df['status'] = np.where(temp_df['rank_diff'] > 0, 'up', None)
+                        temp_df['status'] = np.where(temp_df['rank_diff'] < 0, 'down', temp_df['status'])
+                        temp_df['status'] = np.where(temp_df['rank_diff'] == 0, 'still', temp_df['status'])
+                        temp_df = temp_df.drop(columns=['rank_diff', 'track_rank_old'])
+                        temp_df = temp_df.rename(columns={'track_rank_new':'track_rank'})
+                        # New tracks
+                        new_df = new_df.loc[new_df['track_id'].isin(new_tracks)]
+                        temp_df = pd.concat([temp_df, new_df])
+                        temp_df['status'] = temp_df['status'].fillna('new')
+                        self.result_dict = temp_df.to_dict('records')
+                    self.post_to_oracledb('trending')
         
     def get_track_details(self, track_ids, store_artist_ids=False):
         batch = 50
@@ -199,9 +226,19 @@ class SpotipyScraper:
                     self.result_dict['artist_id'].append(track['artists'][0]['id'])
                 if store_artist_ids:
                     self.artist_ids += self.result_dict['artist_id']
-                self.post_to_oracledb('tracks')
+                flag = self._check_empty_result_dict()
+                if not flag:
+                    self.post_to_oracledb('tracks')
                 time.sleep(3)
-        
+    
+    def _check_empty_result_dict(self):
+        try:
+            if any(self.result_dict.values()):
+                return False
+        except AttributeError as e:
+            if len(self.result_dict) > 0:
+                return False
+        return True    
     def save_to_csv(self, fname="data.csv"):
         if not self.result_dict:
             print('No data was scraped')
@@ -215,26 +252,28 @@ class SpotipyScraper:
         if not self.result_dict:
             print('No data was scraped')
             raise Exception
-        if any(self.result_dict.values()):
-            if not self.oracle_access_token:
-                self._get_oracle_access_token()
+        if not self.oracle_access_token:
+            self._get_oracle_access_token()
+        if isinstance(self.result_dict, dict):
             df = pd.DataFrame(self.result_dict)
             records = df.to_dict('records')
-            records_json = json.dumps({'payload':records})
-            url = f"https://g9c989a618c1148-spotifydb.adb.ap-sydney-1.oraclecloudapps.com/ords/admin/upsert/{endpoint}/"
-            response = requests.post(url, data=records_json, headers={'Authorization': 'Bearer ' + self.oracle_access_token})
-            
-            if response.status_code == 401:
-                print("Token Expired, Getting New Token...")
-                self.oracle_access_token = None
-                self.post_to_oracledb(endpoint)
-            if response.text != '':
-                print(response.text)
-                print('Warning, there was an error upserting into the database, PL/SQL has an error. See above.')
-            if response.status_code != 200:
-                print(response.text)
-                raise Exception
-            print(f"Successfully upserted {len(records)} records to {endpoint}")
+        else:
+            records = self.result_dict
+        records_json = json.dumps({'payload':records})
+        url = f"https://g9c989a618c1148-spotifydb.adb.ap-sydney-1.oraclecloudapps.com/ords/admin/upsert/{endpoint}/"
+        response = requests.post(url, data=records_json, headers={'Authorization': 'Bearer ' + self.oracle_access_token})
+        
+        if response.status_code == 401:
+            print("Token Expired, Getting New Token...")
+            self.oracle_access_token = None
+            self.post_to_oracledb(endpoint)
+        if response.text != '':
+            print(response.text)
+            print('Warning, there was an error upserting into the database, PL/SQL has an error. See above.')
+        if response.status_code != 200:
+            print(response.text)
+            raise Exception
+        print(f"Successfully upserted {len(records)} records to {endpoint}")
 
 load_dotenv()
 # client_secret, client_id = os.getenv("SPOTIPY_CLIENT_SECRET"), os.getenv("SPOTIPY_CLIENT_ID")
@@ -243,9 +282,9 @@ if __name__ == '__main__':
     # artist_ids = data['artist_id'].tolist()[98:]
     collecter = SpotipyScraper(oracle_client_id=os.getenv("ORACLE_CLIENT_ID"), 
                                oracle_client_secret=os.getenv("ORACLE_CLIENT_SECRET"))  
-    p_id = "6UeSakyzhiEt4NB3UAd6NQ" # Billboard 100
-    # p_id = "37i9dQZEVXbMDoHDwVN2tF" # top50 global playlist id
-    collecter.get_playlist(p_id, store_artist_ids=True) 
+    # p_id = "6UeSakyzhiEt4NB3UAd6NQ" # Billboard 100
+    p_id = "37i9dQZEVXbMDoHDwVN2tF" # top50 global playlist id
+    collecter.get_playlist(p_id, update_trending=True, store_artist_ids=True) 
     # for _ in range(10): 
     #     response = requests.get("https://g9c989a618c1148-spotifydb.adb.ap-sydney-1.oraclecloudapps.com/ords/admin/api/tracks/missing")
     #     data = response.json()['items']
@@ -257,10 +296,9 @@ if __name__ == '__main__':
     
     # artist_ids = collecter.artist_ids
     
-    collecter.get_artist_details(collecter.artist_ids)
+    # collecter.get_artist_details(collecter.artist_ids)
     # collecter.save_to_csv('artist_details.csv')
     # collecter.post_to_oracledb('artists')
-
 
 
 
